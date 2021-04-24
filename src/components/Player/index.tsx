70058316e77437cb96f667b34f5ea86db9e17f9f
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2021-04-23 22:25:27 -0300

    Criando pagina de detalhes, roteamento no next.js e criacao da pagina de episodios, usando o conceito de paginas estaticas e dinamicas

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,7 +4,7 @@ export function Player(){
   return(
     <div className={styles.playerContainer}>
       <header>
-        <img src="/playing.svf" alt="Tocando agora"/>
+        <img src="/playing.svg" alt="Tocando agora"/>
         <strong>Tocando Agora</strong>
       </header>
 
